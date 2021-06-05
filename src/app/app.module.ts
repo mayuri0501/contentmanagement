b28a911cba60c39e874ef7c8db9f742182f10c75
Utsav Singh <pokour@gmail.com>
2021-06-05 18:03:43 +0530

    Revert "Authentication component created."
    This reverts commit c06d6d86bc4445d6a0c58140d365e6dc297fc14a.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthenticateComponent } from './authenticate/authenticate.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AuthenticateComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
